d4e47126767596eb6254b0b1f5ea18e77108bd42
Herbert Gomez <gomherbe@amazon.com>
2021-04-18 17:36:29 -0500

    cloud9 setup fix

--- setup/cloud9-setup.sh
+++ setup/cloud9-setup.sh
@@ -7,25 +7,19 @@ sudo yum install -y jq gettext bash-completion moreutils
 sudo curl --silent --location "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/linux_64bit/session-manager-plugin.rpm" -o "session-manager-plugin.rpm"
 sudo yum install -y session-manager-plugin.rpm
 
-#Install kubectl and enable kubectl bash_completion
+#Install kubectl
 sudo curl --silent --location -o /usr/local/bin/kubectl \
    https://amazon-eks.s3.us-west-2.amazonaws.com/1.17.11/2020-09-18/bin/linux/amd64/kubectl
 sudo chmod +x /usr/local/bin/kubectl
-sudo eksctl completion bash >> ~/.bash_completion
-. /etc/profile.d/bash_completion.sh
-. ~/.bash_completion
 
 #Install yq for yaml processing
 echo 'yq() {
   docker run --rm -i -v "${PWD}":/workdir mikefarah/yq "$@"
 }' | tee -a ~/.bashrc && source ~/.bashrc
 
-#Install eksctl and enable eksctl bash_completion
+#Install eksctl
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/latest_release/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv -v /tmp/eksctl /usr/local/bin
-sudo eksctl completion bash >> ~/.bash_completion
-. /etc/profile.d/bash_completion.sh
-. ~/.bash_completion
 
 #Verify the binaries are in the path and executable
 if ! [ -x "$(command -v jq)" ] || ! [ -x "$(command -v envsubst)" ] || ! [ -x "$(command -v kubectl)" ] || ! [ -x "$(command -v eksctl)" ] || ! [ -x "$(command -v ssm-cli)" ]; then
